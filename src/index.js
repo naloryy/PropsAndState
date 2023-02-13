8f02879dd0bf4ca41f9c6c1a14a1d9e2e12b24c7
Nalory01 <Panalory@gmail.com>
2023-02-13 13:28:17 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // write your Color component here
-
+const Color = (props) => {
+  return (
+    <div className = {props.color} />
+  )
+}
 const Picker = () => {
   return (
     <div id="container">
@@ -10,7 +14,11 @@ const Picker = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+      <Color color = 'blue' />
+      <Color color = 'black'/>
+      <Color color = 'violet'/>
+      </div>
     </div>
   );
 }
